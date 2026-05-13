b6a5e30f00d7361696a1b57eb1e0fe8cc1d0db09
Daniel de jesus (aka:Falker) <danielmeloko@gmail.com>
2026-05-13 17:13:36 -0300

    Second Update code

--- src/main.cpp
+++ src/main.cpp
@@ -1,86 +1,115 @@
 #include <Arduino.h>
 
-#define pinoRELE 2
+#define pinoRELE 4
+
 #define pinoLDR 34
 
- // CALIBRAÇÂO DAS VARIAVES LDR 
-const int LDR_ALIVE = 2200;
-const int LDR_KEEP_GOING = 1800;
+// CALIBRAÇÂO DAS VARIAVES LDR
+
+const int LDR_NOpeople = 2200;
+
+const int LDR_YESpeople = 1800;
+
 // --------------------------------
 
-
 unsigned long CLOCKanterior = 0;
+
 const int CLOCKwait_test = 1000;
 
-enum states{Iniciado, Aron, Aroff, Listening};
+
+enum states{
+  Iniciado, 
+  Aron, 
+  Aroff, 
+  Listening
+  };
+
 states state_now = Iniciado;
+
 bool relay_state = false;
 
-
 void setup() {
+
+  digitalWrite(pinoRELE, LOW);
+
   pinMode(pinoRELE, OUTPUT);
+
   pinMode(pinoLDR, INPUT);
-  Serial.begin(9600);
-  Serial.println("Aroff system initialized.");
-  Serial.println("System produced by LAARC 2026");
-  
+
+  Serial.begin(115200);
+  delay(1000); //tem que tirar esse delay >:( 
+
 }
 
-void Aroff_line(){
+// ---- Principal ---
+
+void Aroff_line(){ // abstrair mais essa parte
+
   const int proof_person = analogRead(pinoLDR);
-  
-  unsigned long CLOCKtime = millis(); // abstrair mais essa parte
-  
+
+  unsigned long CLOCKtime = millis(); 
+
+// Parte de Depuração para o SERIAL >:) //
+
+  static unsigned long lastDATA_SENSOR = 0;
+    if (CLOCKtime - lastDATA_SENSOR > 500){
+    Serial.print("Sensor LDR : "); 
+    Serial.print(proof_person);
+    Serial.print(" | State: "); 
+    Serial.println(state_now == Aron ? "ARON (RELAY ON/NO)": state_now == Aroff ? "AROFF (RELAY OFF/NC)" : "LISTENING");
+    lastDATA_SENSOR = CLOCKtime;
+}
+/////////////////////////////////////////
+
   switch(state_now){
-    
+
     case Iniciado:
-  
+      
+      Serial.println("Aroff system initialized.");
+      Serial.println("System produced by LAARC 2026");
+
       if(CLOCKtime - CLOCKanterior >= CLOCKwait_test){
-        digitalWrite(pinoRELE, LOW);
-        Serial.println("Relay is Online");
-        CLOCKanterior = CLOCKtime;
+        Serial.println("Relay is Online and Listening ... ");
+
         state_now = Listening;
-      }else{
-        Serial.println("Relay is Offline");
-        digitalWrite(pinoRELE, HIGH);
-      }
-    break;
-
-    case Listening:
-        if(proof_person >= LDR_ALIVE){
-
-          Serial.println("Person Detected");
-          state_now = Aron;
-      } else if(proof_person < LDR_KEEP_GOING){
-          Serial.println("no person detected");
-          state_now = Aroff;
-      }
+}
       break;
 
-    case Aron:
+  case Listening:
+
+    if(proof_person < LDR_YESpeople){
+      Serial.println("Person Detected");
+      state_now = Aron;
+
+    } else if(proof_person < LDR_NOpeople){
+      Serial.println("no person detected");
+      state_now = Aroff;
+}
+    break;
+
+  case Aron:
+    digitalWrite(pinoRELE, HIGH);
+
+    if(proof_person >= LDR_NOpeople){
       digitalWrite(pinoRELE, LOW);
+     state_now = Aroff;
+}
 
-      if(proof_person < LDR_KEEP_GOING){
-        digitalWrite(pinoRELE, HIGH);
-        state_now = Aroff;
-      }
     break;
 
-    case Aroff:
+  case Aroff:
+    digitalWrite(pinoRELE, LOW);
+   
+    if(proof_person < LDR_YESpeople){
       digitalWrite(pinoRELE, HIGH);
-      
-      if(proof_person >= LDR_ALIVE){
-        digitalWrite(pinoRELE, LOW);
-        state_now = Aron;
-      }
-    break;
+      state_now = Aron;
+
   }
+    break;
+ }
 }
-
 void loop() {
-  Aroff_line();
-  //depois detalhar mais para condição caso a inicialização falhe
-  //ou a conexão entre as outras esp seja notada
+ Aroff_line();
+ //depois detalhar mais para condição caso a inicialização falhe 
+ //ou a conexão entre as outras esp seja notada
 }
-
-
